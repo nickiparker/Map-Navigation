874be9a788815595a4bc9fb7d18acb0b1ec880fb
Nicki Parker <NP204409@falmouth.ac.uk>
2018-11-21 18:20:21 +0000

    Initial Commit

---
+++ map-navigation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  map-navigation
+//
+//  Created by JODIE PARKER on 21/11/2018.
+//  Copyright © 2018 Nickiparker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
